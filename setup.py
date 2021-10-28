1334b31792544e01484da0b6ae8994d98dfb3940
Oldrich Smehlik <oldrich@smehlik.net>
2021-10-28 03:05:32 +0200

    Doc: Updated setup.py, added classifiers, license, url, description

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="activities",
+    name='activities',
+    description='A simple command line time tracker',
     version='0.0.1',
-    author="Oldřich Šmehlík",
-    author_email="oldrich@smehlik.net",
+    license='BSD 2-Clause License',
+    author='Oldřich Šmehlík',
+    author_email='oldrich@smehlik.net',
+    url='https://github.com/osmehlik/activities',
     packages=find_packages(include=['activities', 'activities.*']),
     entry_points={
         'console_scripts': ['activities-cli=activities.app:main']
-    }
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'
+    ]
 )
