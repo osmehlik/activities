86e9aa7476f31bcea492e656cad63b815a391f35
Oldrich Smehlik <oldrich@smehlik.net>
2021-12-05 02:36:50 +0100

    Refactor: Remove unnecessary conversion to int

--- activities/time.py
+++ activities/time.py
@@ -5,15 +5,15 @@ from datetime import timedelta
 
 
 def extract_total_days_value(td: timedelta) -> float:
-    return int(td.total_seconds()) / NUM_SECONDS_IN_DAY
+    return td.total_seconds() / NUM_SECONDS_IN_DAY
 
 
 def extract_total_hours_value(td: timedelta) -> float:
-    return int(td.total_seconds()) / NUM_SECONDS_IN_HOUR
+    return td.total_seconds() / NUM_SECONDS_IN_HOUR
 
 
 def extract_total_minutes_value(td: timedelta) -> float:
-    return int(td.total_seconds()) / NUM_SECONDS_IN_MINUTE
+    return td.total_seconds() / NUM_SECONDS_IN_MINUTE
 
 
 def extract_total_days_str(td: timedelta) -> str:
