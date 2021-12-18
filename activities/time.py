414f79601340738f4455e49fabd612a4ebb02946
Oldrich Smehlik <oldrich@smehlik.net>
2021-12-18 23:27:59 +0100

    Refactor: rename extract_total_{days,hours,minutes}_value -> extract_total_{days_hours_minutes}

--- activities/time.py
+++ activities/time.py
@@ -4,28 +4,28 @@ from activities.constants import NUM_SECONDS_IN_DAY, \
 from datetime import timedelta
 
 
-def extract_total_days_value(td: timedelta) -> float:
+def extract_total_days(td: timedelta) -> float:
     return td.total_seconds() / NUM_SECONDS_IN_DAY
 
 
-def extract_total_hours_value(td: timedelta) -> float:
+def extract_total_hours(td: timedelta) -> float:
     return td.total_seconds() / NUM_SECONDS_IN_HOUR
 
 
-def extract_total_minutes_value(td: timedelta) -> float:
+def extract_total_minutes(td: timedelta) -> float:
     return td.total_seconds() / NUM_SECONDS_IN_MINUTE
 
 
 def extract_total_days_str(td: timedelta) -> str:
-    value = extract_total_days_value(td)
+    value = extract_total_days(td)
     return "{0:.2f}".format(value)
 
 
 def extract_total_hours_str(td: timedelta) -> str:
-    value = extract_total_hours_value(td)
+    value = extract_total_hours(td)
     return "{0:.2f}".format(value)
 
 
 def extract_total_minutes_str(td: timedelta) -> str:
-    value = extract_total_minutes_value(td)
+    value = extract_total_minutes(td)
     return "{0:.0f}".format(value)
